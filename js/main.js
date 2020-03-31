ac2ba14ad220743eb8b3ab5142ecc3960d2c0a27
chanmaoooo <chanmaoooo@gmail.com>
2020-03-31 16:19:58 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,86 @@
+"use strict";
+
+{
+  const question = document.getElementById("question");
+  const choices = document.getElementById("choices");
+  const btn = document.getElementById("btn");
+  const result = document.getElementById("result");
+  const scoreLabel = document.querySelector("#result > p");
+
+  const quizSet = shuffle([
+    {q: "覚えられないのは？",　c: ["サザンドラの「10まんボルト」", "ガブリアスの「かえんほうしゃ」", "ボーマンダの「ハイドロポンプ」"]},
+    {q: "「クラブハンマー」「あくうせつだん」「リーフブレード」に共通する特徴は？", c: ["急所に当たりやすい", "命中率が100ではない", "物理技である"]},
+    {q: "オクタンが覚えられないのは？", c: ["どろばくだん", "ヘドロばくだん", "タネばくだん"]},
+    {q: "追加効果が他と違うのは？", c:["アイスボール", "シャドーボール", "エナジーボール"]},
+    {q: "特殊技はどれ？", c:["まとわりつく", "まきつく", "からみつく"]}
+  ]);
+  let currentNum = 0;
+  let isAnswered;
+  let score = 0;
+
+
+
+  function shuffle(arr) {
+    for(let i = arr.length -1; i>0; i--) {
+      const j = Math.floor(Math.random()*(i+1));
+      [arr[j], arr[i]] = [arr[i], arr[j]];
+    }
+    return arr;
+  }
+
+
+  function checkAnswer(li) {
+    if(isAnswered) return;
+
+    isAnswered = true;
+    if(li.textContent === quizSet[currentNum].c[0]) {
+      li.classList.add("correct");
+      score++;
+    }
+    else li.classList.add("wrong");
+
+    btn.classList.remove("disabled");
+  }
+
+
+  function setQuiz() {
+    isAnswered = false;
+    question.textContent = quizSet[currentNum].q;
+    
+    while(choices.firstChild) {
+      choices.removeChild(choices.firstChild);
+    }
+    
+    const shuffledChoices = shuffle([...quizSet[currentNum].c]);
+    shuffledChoices.forEach(choice => {
+      const li = document.createElement("li");
+      li.textContent = choice;
+      li.addEventListener("click", ()=> {
+        checkAnswer(li);
+      });
+
+      choices.appendChild(li);
+    });
+
+    if(currentNum === quizSet.length-1) btn.textContent = "Show Score";
+  }
+
+  setQuiz();
+
+
+  btn.addEventListener("click", ()=> {
+    if(btn.classList.contains("disabled")) return;
+    btn.classList.add("disabled");
+
+    if(currentNum === quizSet.length-1) {
+      // console.log(`${score}/${quizSet.length}`);
+      scoreLabel.textContent = `${score}/${quizSet.length}`;
+      result.classList.remove("hidden");
+    }
+    else {
+      currentNum++;
+      setQuiz();
+    }
+  });
+
+}
